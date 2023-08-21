5e74ca6a199c20c3d4b51b6598961f3adde2e3c4
duartevitu-alt <vitordduarte95@gmail.com>
2023-08-21 11:41:53 +0100

    Updated breakfest menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My parents new healthy diner")
+
+streamlit.header("Breakfest menu")
+streamlit.text("Omega 3 and Blueberry Oatmeal")
+streamlit.text("Kale, Spinach and Rocket smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
